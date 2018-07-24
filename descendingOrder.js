fc1d8b1c3747c91d2eefb89653622a153af089bc
Carlos Perez <solracias@gmail.com>
2018-07-23 21:38:09 -0700

    added comments

--- descendingOrder.js
+++ descendingOrder.js
@@ -1,18 +1,33 @@
 function descendingOrder(n) {
+  // create empty array
   let arr = [];
+
+  // convert parameter to a string
   let num = n.toString();
 
+  // loop through each index and push to array
   for (var i = 0; i < num.length; i += 1) {
     arr.push(+num.charAt(i));
   }
 
+  // sort in ascending order
   arr.sort();
+
+  // sort in descending order
   arr.reverse();
+
+  // make the array into a string. 
   let descendingString = arr.join("");
+
+  // make string into an integer
   let finalNumber = parseInt(descendingString);
 
-
-  console.log(finalNumber);
+  if (isNaN(finalNumber)) {
+    console.log('not a number');
+  } else {
+    console.log(finalNumber);
+  }
+  
 }
 
-descendingOrder(43234);
+descendingOrder(234);
